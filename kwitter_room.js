09df7d12df7eb3ca062f74a701632bd0e648630e
Rehaan-Kumar <73431008+Rehaan-Kumar@users.noreply.github.com>
2021-05-15 23:27:00 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDiKybzTv4y6wnGcOHO4-dhEAsWXgNxHF8",
+    authDomain: "kwitter-chat-app-project-71bae.firebaseapp.com",
+    databaseURL: "https://kwitter-chat-app-project-71bae-default-rtdb.firebaseio.com",
+    projectId: "kwitter-chat-app-project-71bae",
+    storageBucket: "kwitter-chat-app-project-71bae.appspot.com",
+    messagingSenderId: "246886927000",
+    appId: "1:246886927000:web:f76bf804594d70c5941eed"
+  };
+  firebase.initializeApp(firebaseConfig);
